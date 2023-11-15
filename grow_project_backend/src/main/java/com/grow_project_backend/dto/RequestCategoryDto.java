5aa83c96d9e645d2de06f96859b26256dfa70554
Jihwan-Hong <flareseek@gmail.com>
2023-11-15 23:55:59 +0900

    [Fix] HOXFIX

--- grow_project_backend/src/main/java/com/grow_project_backend/dto/RequestCategoryDto.java
+++ grow_project_backend/src/main/java/com/grow_project_backend/dto/RequestCategoryDto.java
@@ -2,11 +2,13 @@ package com.grow_project_backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RequestCategoryDto {
-    public String tab;
+    private String tab;
 }
